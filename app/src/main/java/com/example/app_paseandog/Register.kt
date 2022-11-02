58d3c4c912be1ab423cbf77fb78ff2091e00331e
julianaeg <julianaecheverrig@gmail.com>
2022-11-02 08:44:44 -0500

    ajuste usuer

--- app/src/main/java/com/example/app_paseandog/Register.kt
+++ app/src/main/java/com/example/app_paseandog/Register.kt
@@ -3,13 +3,21 @@ package com.example.app_paseandog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.app_paseandog.databinding.ActivityRegistroBinding
+import com.example.app_paseandog.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
-    private lateinit var binding:ActivityRegistroBinding
+    private lateinit var binding:ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_register)
+
+        binding=ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFoto.setOnClickListener {
+            startActivity(Intent(this,CargarFoto::class.java))
+        }
 
         binding.btnInicio.setOnClickListener{
             startActivity(Intent(this,MainActivity::class.java))
